2bf688bdd9ef27e5abc41e06e7ee5612d2788ced
Vanessa Würdinger <vanessa.wuerdinger@gmx.de>
2023-01-05 21:52:38 +0100

    responsive design for about me and skills added

--- src/app/about-me/highlight-image.directive.ts
+++ src/app/about-me/highlight-image.directive.ts
@@ -4,16 +4,25 @@ import { Directive, ElementRef, HostListener, Input, OnInit, Renderer2 } from '@
   selector: '[appHighlightImage]'
 })
 export class HighlightImageDirective {
+  showHighlight = false;
 
   constructor(private elementRef: ElementRef, private renderer: Renderer2) {}
 
-  @Input('appHighlightImage') hoverClass: any;
+  // @Input('appHighlightImage') hoverClass: any;
 
-  @HostListener('mouseenter') onMouseEnter() {
-      this.elementRef.nativeElement.classList.add(this.hoverClass);
-  }
+  // @HostListener('click') onClick() {
+  //   if (!this.showHighlight) {
+  //     this.renderer.addClass(this.elementRef, this.hoverClass);
+  //   } else {
+  //     this.renderer.removeClass(this.elementRef, this.hoverClass);
+  //   }
+  //   this.showHighlight = !this.showHighlight
+  // }
 
-  @HostListener('mouseleave') onMouseLeave() {
-      this.elementRef.nativeElement.classList.remove(this.hoverClass);
-  }
+  // @HostListener('touchmove') onTouchMove() {
+  //     this.elementRef.nativeElement.classList.add(this.hoverClass);
+  // }
 }
+
+
+
