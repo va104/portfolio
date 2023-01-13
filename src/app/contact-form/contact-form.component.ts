c571cde3813c64fe2dcba993e059ca96ba350630
Vanessa Würdinger <vanessa.wuerdinger@gmx.de>
2023-01-14 00:58:40 +0100

    contact form adjusted

--- src/app/contact-form/contact-form.component.ts
+++ src/app/contact-form/contact-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-contact-form',
@@ -10,6 +11,7 @@ export class ContactFormComponent implements OnInit {
   @ViewChild('myForm') myForm: ElementRef; 
   @ViewChild('nameField') nameField: ElementRef; 
   @ViewChild('messageField') messageField: ElementRef; 
+  @ViewChild('mail') mail: ElementRef; 
   @ViewChild('sendButton') sendButton: ElementRef; 
   constructor() { }
 
@@ -20,21 +22,23 @@ export class ContactFormComponent implements OnInit {
   //Angular Animation für Say Hi Header
   // soll alle 2 Sekunden seine Farbe von Orange nach hell wechseln
 
-  async sendMail() {
+  async sendMail(form: NgForm) {
     //action="https://vanessa-wuerdinger.de/send_mail/send_mail.php"
     //ftp://f015573b@vanessa-wuerdinger.de/vanessa-wuerdinger.de/assets/libs/send_mail/send_mail.php
-    console.log('sending mail', this.myForm);
-    let nameField = this.nameField.nativeElement;
-    let messageField = this.messageField.nativeElement;
-    let sendButton = this.sendButton.nativeElement;
-    nameField.disabled = true;
-    messageField.disabled = true;
-    sendButton.disabled = true;
+    console.log('sending mail', form.value);
+    // let nameField = this.nameField.nativeElement;
+    // let messageField = this.messageField.nativeElement;
+    // let mail = this.mail.nativeElement;
+    // let sendButton = this.sendButton.nativeElement;
+    // nameField.disabled = true;
+    // messageField.disabled = true;
+    // sendButton.disabled = true;
     //Ladeanimation anzeigen, dass gesendet wird 
 
-    let fd = new FormData();
-    fd.append('name', nameField.value);
-    fd.append('message', messageField.value);
+    const fd = new FormData();
+    fd.append('name', form.value.name);
+    fd.append('message', form.value.message);
+    // fd.append('mail', mail.value);
     await fetch('https://vanessa-wuerdinger.de/assets/libs/send_mail/send_mail.php',
       {
         method: 'POST',
@@ -42,9 +46,9 @@ export class ContactFormComponent implements OnInit {
       })
 
       //Text anzeigen, Nachricht gesendet.
-    nameField.disabled = false;
-    messageField.disabled = false;
-    sendButton.disabled = false;
+    // nameField.disabled = false;
+    // messageField.disabled = false;
+    // sendButton.disabled = false;
   }
 
 
